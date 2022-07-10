ffa373245a7051e130d847543b563c7290b0f2ff
wasiuralukasz <lkasz6233@gmail.com>
2022-07-10 21:54:34 +0200

    add portfolio section and scroll navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 let navbar = window.document.querySelector(".navbar");
 
+//navbar
 addEventListener('scroll', () => {
     if(window.scrollY > 20)
         navbar.classList.add("navbar__scroll");
     else
         navbar.classList.remove("navbar__scroll");
 })
+
+//one scroll = 100vh
+// let currentScroll;
+// let windowHeight = window.innerHeight;
+
+// addEventListener('wheel', (e) => {
+//     currentScroll = document.documentElement.scrollTop;
+//     console.log(e);
+//     if(e.wheelDelta < 0)
+//         window.scrollTo(0, currentScroll + windowHeight); 
+//     else 
+//         window.scrollTo(0, currentScroll - windowHeight); 
+// });
