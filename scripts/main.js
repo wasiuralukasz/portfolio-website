b9230429ccf5a2e79791a23f31a45397de841214
wasiuralukasz <lkasz6233@gmail.com>
2022-07-14 23:57:39 +0200

    add contact section and fade in animation to portfolio section

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,10 @@
 let navbar = window.document.querySelector(".navbar");
 
 //navbar
-addEventListener('scroll', () => {
-    if(window.scrollY > 20)
-        navbar.classList.add("navbar__scroll");
-    else
-        navbar.classList.remove("navbar__scroll");
-})
+addEventListener("scroll", () => {
+  if (window.scrollY > 20) navbar.classList.add("navbar__scroll");
+  else navbar.classList.remove("navbar__scroll");
+});
 
 //one scroll = 100vh
 // let currentScroll;
@@ -16,7 +14,26 @@ addEventListener('scroll', () => {
 //     currentScroll = document.documentElement.scrollTop;
 //     console.log(e);
 //     if(e.wheelDelta < 0)
-//         window.scrollTo(0, currentScroll + windowHeight); 
-//     else 
-//         window.scrollTo(0, currentScroll - windowHeight); 
+//         window.scrollTo(0, currentScroll + windowHeight);
+//     else
+//         window.scrollTo(0, currentScroll - windowHeight);
 // });
+
+let projectItems = document.querySelectorAll(".portfolio__grid-item");
+let projectButton = document.querySelector("portfolio__button");
+let delay = 0;
+
+projectItems.forEach((item) => {
+  item.style.transition = `all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) ${delay}s, opacity 0.5s cubic-bezier(0.645, 0.045, 0.355, 1) ${delay}s, transform 0.5s cubic-bezier(0.645, 0.045, 0.355, 1) ${delay}s`;
+  delay += 0.1;
+});
+
+window.addEventListener("scroll", () => {
+  let trigger = (window.innerHeight / 6) * 4;
+
+  projectItems.forEach((item) => {
+    let topDistance = item.getBoundingClientRect().top;
+
+    if (trigger > topDistance) item.classList.add("show");
+  });
+});
