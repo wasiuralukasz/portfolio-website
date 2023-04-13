5f2346da9c8a95d09894d6560f14c7e860cfbca0
wasiuralukasz <lkasz6233@gmail.com>
2023-04-13 16:41:58 +0200

    Added shine animation on project item

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+//Download data from GitHub and create new portfolio item
+let portfolioList = document.getElementById('portfolio__list');
+const URL = "https://api.github.com/users/wasiuralukasz"
+
+getData();
+
 let navbar = window.document.querySelector(".navbar");
 
 //navbar
@@ -55,4 +61,87 @@ navbarLinks.forEach((link) => {
   });
 })
 
+//Get list of public repositories from GitHub
+async function getData() {
+  const response = await fetch(`${URL}/repos`);
+  const jsonData = await response.json();
+  let counter = 0;
+  
+  //Create project list in portfolio section 
+  jsonData.map((element) => {
 
+      if (counter > 2) {
+          portfolioList.innerHTML +=
+              `
+          <li class="portfolio__grid-item hidden">
+            <div class="project__container">
+                <span class="project__header">
+                <h2 class="project__tittle">${element.name}</h2>
+                  <div>
+                  <a class="project__link" href="${element.html_url}">
+                      <img src="/images/Github-logo.svg" alt="" />
+                    </a>
+                    <a class="project__link" href="">
+                      <img src="/images/external-link.svg" alt="" />
+                    </a>
+                  </div>
+                </span>
+                <p class="project__description">
+                  ${element.description}
+                </p>
+                <ul class="project__tech-list">
+                  <li>${element.language}</li>
+                </ul>
+                <div class="shine" data-target="project__shine"></div>
+            </div>
+          </li>
+      `
+      }
+      else {
+          portfolioList.innerHTML +=
+              `
+          <li class="portfolio__grid-item">
+            <div class="project__container">
+                <span class="project__header">
+                <h2 class="project__tittle">${element.name}</h2>
+                  <div>
+                  <a class="project__link" href="${element.html_url}">
+                      <img src="/images/Github-logo.svg" alt="" />
+                    </a>
+                    <a class="project__link" href="">
+                      <img src="/images/external-link.svg" alt="" />
+                    </a>
+                  </div>
+                </span>
+                <p class="project__description">
+                  ${element.description}
+                </p>
+                <ul class="project__tech-list">
+                  <li>${element.language}</li>
+                </ul>
+                <div class="shine" data-target="project__shine"></div>
+            </div>
+          </li>
+      `
+      }
+      counter = counter + 1;
+      
+  })
+
+  let projectItems = document.querySelectorAll('.portfolio__grid-item');
+
+  projectItems.forEach((item) => {
+    item.addEventListener('mouseenter', (e) => {
+      item.children[0].childNodes[7].classList.add('shine-active');
+    });
+
+    item.addEventListener('mousemove', (e) => {
+      item.children[0].childNodes[7].style.setProperty('transform', `translate(${e.offsetX / 2}px, ${e.offsetY / 2}px)`)
+    });
+
+    item.addEventListener('mouseleave', (e) => {
+      item.children[0].childNodes[7].classList.remove('shine-active');
+
+    });
+  })
+}
